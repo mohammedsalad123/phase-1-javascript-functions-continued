a50c8933835d6fba4f283d84675ecf21e23870a9
mohammed <mohammed.salad@student.moringaschool.com>
2023-10-05 08:42:09 -0400

    done with

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//code your solution here
 // code your solution here
+function saturdayFun(event = 'roller-skate'){
+    return `This Saturday, I want to ${event}!`
+
+}
+
+saturdayFun("bathe my dog");
+
+function mondayWork(x = 'go to the office'){
+    return `This Monday, I will ${x}.`;
+}
+
+mondayWork('work from home')
+
+function wrapAdjective(wrapper = '*') {
+    return function result(adjective = '||') {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
